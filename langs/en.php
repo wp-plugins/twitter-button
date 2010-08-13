ea5bac4dec142b6565e8803f4d526c367bc1669b
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-13 11:08:56 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/trunk@275240 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/en.php
@@ -0,0 +1,51 @@
+<?php
+// English language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Button Settings',
+	'plugin' 			=> 'Plugin Settings',
+	'saved' 			=> 'Settings saved.',
+	'update' 			=> 'Submit',
+	
+	// Button Settings
+	'vertical' 			=> 'Vertical count',
+	'horizontal' 		=> 'Horizontal count',
+	'none' 				=> 'No count',
+	
+	'tweet-text' 		=> 'Tweet text',
+	'tweet-text-desc' 	=> 'This is the text that people will include in their Tweet when they share from your website:',
+	'tweet-text-1' 		=> 'The title of the page the button is on.',
+	'tweet-text-exmp' 	=> 'Example: Check this out!',
+	
+	'url-desc' 			=> 'Suggest a default Tweet for users.',
+	'url-1' 			=> 'The URL for the page the button is on.',
+	
+	'language' 			=> 'Language',
+	'language-desc' 	=> 'This is the language that the button will render in on your website. People will see the Tweet dialog in their selected language for Twitter.com.',
+	
+	'user' 				=> 'Twitter account',
+	'user-desc' 		=> 'Recommend Twitter account for users to follow after they share content from your website.',
+	'user-1' 			=> 'This user will be @ mentioned in the suggested Tweet',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Admin panel language',
+	
+	'method' 			=> 'Method',
+	'automatic' 		=> 'Automatic',
+	'manual' 			=> 'Manual',
+	'manual_desc' 		=> 'With manual method you can put the tweet button wherever you like. All you have to do is to insert this code "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" in your theme.',
+	'shortcode' 		=> 'Shortcode',
+	'shortcode_desc' 	=> 'If you want to disaplay the Twitter Button in certain pages, all you have to do is to insert this shortcode "<strong>[twitter-button]</strong>" in your blog post or page.',
+	'example' 			=> 'Example:',
+	
+	// Restore
+	'restore' 			=> 'Restore',
+	'restore-desc' 		=> 'Use this option to restore all the default settings.',
+	
+	// FYI
+	'author' 			=> 'Plugin Author',
+	'version' 			=> 'Version',
+	'url' 				=> 'Plugin URL',
+	'donate' 			=> 'Donate to this plugin'
+); 
+?>
