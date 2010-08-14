2bc7b848b125f2a63f8c077b94efe7476557eec3
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-14 14:08:52 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/tags/0.2@275807 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/bg.php
@@ -0,0 +1,68 @@
+<?php
+// Bulgarian language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Настройки на бутона',
+	'plugin' 			=> 'Настройки на плъгина',
+	'saved' 			=> 'Промените бяха запазени.',
+	'update' 			=> 'Запази',
+	
+	// Button Settings
+	'vertical' 			=> 'Вертикален брояч',
+	'horizontal' 		=> 'Хоризонтален брояч',
+	'none' 				=> 'Без брояч',
+	
+	'tweet-text' 		=> 'Текст на туита',
+	'tweet-text-desc' 	=> 'Това е съобщението, което ще бъде включено в туита при споделяне:',
+	'tweet-text-1' 		=> 'Заглавието на публикацията',
+	'tweet-text-2' 		=> 'Заглавието на публикацията + името на блога',
+	'tweet-text-3' 		=> 'Името на блога',
+	'tweet-text-4' 		=> 'Друго съобщение',
+	'tweet-text-exmp' 	=> 'Пример: Вижте това!',
+	
+	'url-desc' 			=> 'URL адресът на страницата, която искате да споделите:',
+	'url-1' 			=> 'Адресът на страницата на която бутонът се намира.',
+	
+	'language' 			=> 'Език',
+	'language-desc' 	=> 'This is the language that the button will render in on your website. People will see the Tweet dialog in their selected language for Twitter.com.',
+	
+	'user' 				=> 'Twitter акаунт',
+	'user-desc' 		=> 'Recommend Twitter account for users to follow after they share content from your website.',
+	'user-1' 			=> 'Потребителското име ще бъде споменато (@ mentioned) в туита',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Език на админ панела',
+	
+	'method' 			=> 'Режим на показване',
+	'automatic' 		=> 'Автоматичен',
+	'manual' 			=> 'Ръчен',
+	'manual_desc' 		=> 'При ръчен режим можете да поставите бутоните, където си пожелаете в темплейта, чрез поставянето на кодът "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>"',
+	'shortcode' 		=> 'Къс код',
+	'shortcode_desc' 	=> 'При къс код <em>(shortcode)</em>, Вие сами контролирате на кои страници да се показват бутоните, като поставите следният код "<strong>[twitter-button]</strong>" в полето за писане на публикация или страница.',
+	'example' 			=> 'Пример:',
+	'exclude' 			=> 'Изключване',
+	'exclude-desc' 		=> 'От тук можете да изберете в кои части от сайта бутонът да не се показва:',
+	'exclude-1' 		=> 'Начална страница',
+	'exclude-2' 		=> 'Публикация',
+	'exclude-3' 		=> 'Страница',
+	'exclude-4' 		=> 'Етикет (tag)',
+	'exclude-5' 		=> 'Категория',
+	'exclude-6' 		=> 'Архив - дата',
+	'exclude-7' 		=> 'Архив - година',
+	'exclude-8' 		=> 'Архив - месец',
+	'exclude-9' 		=> 'Архив - ден',
+	'exclude-10' 		=> 'Архив - час',
+	'exclude-11' 		=> 'Търсене',
+	'exclude-12' 		=> 'RSS',
+	
+	// Restore
+	'restore' 			=> 'Възстановяване',
+	'restore-desc' 		=> 'Ако натиснете бутона "Възстановяване", всички настройки ще бъдат по подразбиране, а вашите промени ще бъдат премахнати.',
+	
+	// FYI
+	'author' 			=> 'Автор на плъгина',
+	'version' 			=> 'Версия',
+	'url' 				=> 'Адрес на плъгина',
+	'donate' 			=> 'Donate to this plugin'
+); 
+?>
