af2f3c023811de4716164ea0c8872a4ebd0f83f0
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-19 08:39:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/tags/0.2.7@278146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/ro.php
@@ -0,0 +1,85 @@
+<?php
+// Romanian language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Setari Buton',
+	'plugin' 			=> 'Setari Plugin',
+	'saved' 			=> 'Setarile au fost salvate.',
+	'update' 			=> 'Salveaza',
+	
+	// Button Settings
+	'vertical' 			=> 'Numaratoare verticala',
+	'horizontal' 		=> 'Numaratoare orizontala',
+	'none' 				=> 'Fara numaratoare',
+	
+	'tweet-text' 		=> 'Textul Tweetului',
+	'tweet-text-desc' 	=> 'Acesta este textul pe care oamenii il vor include in Tweetul lor cand vor da share de pe website-ul tau.',
+	'tweet-text-1' 		=> 'Titlul postului',
+	'tweet-text-2' 		=> 'Titlul postului + numele blogului',
+	'tweet-text-3' 		=> 'Numele blogului',
+	'tweet-text-4' 		=> 'Mesaj personalizat',
+	'tweet-text-exmp' 	=> 'Exemplu: Citeste asta!',
+	
+	'url-desc' 			=> 'Sugereaza un Tweet initial pentru utilizatori.',
+	'url-1' 			=> 'Adresa paginii pe care se afla butonul.',
+	
+	'language' 			=> 'Limba',
+	'language-desc' 	=> 'Aceasta este limba in care va aparea butonul pe pagina ta. Utilizatorii vor vedea Tweetul in limba selectata pentru Twitter.com',
+	
+	'user' 				=> 'Cont Twitter',
+	'user-desc' 		=> 'Recomanda contul de Twitter pentru utilizatori ca sa dea follow dupa ce vor da share continutului de pe website-ul tau.',
+	'user-1' 			=> 'Acest user va fi mentionat @ in Tweetul sugerat',
+	
+	'recommend' 		=> 'Recomanda un utilizator de Twitter',
+	'recommend-desc' 	=> 'Recomanda un alt cont de Twitter pe care utilizatorii sa-l urmareasca dupa ce dau share continutului de pe site-ul tau.',
+	'recommend-desc-1' 	=> 'Descrierea utilizatorului de Twitter recomandat ',
+	
+	'position' 			=> 'Pozitia butonului',
+	'position-desc' 	=> 'Aceasta optiune seteaza locul unde butonul de Twitter va fi afisat.',
+	'position-1' 		=> 'La inceputul postului',
+	'position-2' 		=> 'La sfarsitul postului',
+	'position-3' 		=> 'Ambele (inceput + sfarsit)',
+	
+	'style' 			=> 'Stilul butonului',
+	'style-desc' 		=> 'Stiluri aditionale pentru elementul div ce inconjoara butonul',
+	'style-example' 	=> '<b>De exemplu:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Limba panou de administrare',
+	
+	'method' 			=> 'Metoda de afisare',
+	'automatic' 		=> 'Automat',
+	'manual' 			=> 'Manual',
+	'manual_desc' 		=> 'Cu metoda manuala poti sa pui butonul unde vrei. Tot ce ai de facut este sa introduci codul "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" in tema ta.',
+	'shortcode' 		=> 'Cod scurt',
+	'shortcode_desc' 	=> 'Daca vrei sa afisezi butonul Twitter doar in anumite pagini, tot ce ai de facut este sa introduci acest cod scurt "<strong>[twitter-button]</strong>" in cadrul postului sau al paginii.',
+	'example' 			=> 'Exemplu:',
+	'exclude' 			=> 'Exclude',
+	'exclude-desc' 		=> 'Exclude butonul Twitter de pe anumite pagini:',
+	'exclude-1' 		=> 'Pagina principala',
+	'exclude-2' 		=> 'Post singular',
+	'exclude-3' 		=> 'Pagina',
+	'exclude-4' 		=> 'Tag',
+	'exclude-5' 		=> 'Categorie',
+	'exclude-6' 		=> 'Arhiva - data',
+	'exclude-7' 		=> 'Arhiva - anul',
+	'exclude-8' 		=> 'Arhiva - luna',
+	'exclude-9' 		=> 'Arhiva - ziua',
+	'exclude-10' 		=> 'Arhiva - ora',
+	'exclude-11' 		=> 'Cautare',
+	'exclude-12' 		=> 'RSS',
+	
+	'shorturl' 			=> 'Serviciu de scurtat URL-uri',
+	'shorturl-desc' 	=> 'Alege serviciul prin care sa scurtezi linkurile sharuite prin butonul de Twitter <em>(momentan se foloseste serviciul oficial Twitter t.co)</em>',
+	
+	// Restore
+	'restore' 			=> 'Setari initiale',
+	'restore-desc' 		=> 'Foloseste aceasta optiune ca sa revii la setarile initiale.',
+	
+	// FYI
+	'author' 			=> 'Autor Plugin',
+	'version' 			=> 'Versiune',
+	'url' 				=> 'Adresa Plugin',
+	'donate' 			=> 'Doneaza pentru acest plugin'
+); 
+?>
