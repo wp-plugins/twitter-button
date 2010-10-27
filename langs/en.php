aecb5e71899111af91544ec2d5cb897accad82c3
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 15:06:00 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/tags/0.2.8@305001 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/en.php
@@ -0,0 +1,86 @@
+<?php
+// English language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Button Settings',
+	'plugin' 			=> 'Plugin Settings',
+	'saved' 			=> 'Settings saved.',
+	'update' 			=> 'Submit',
+	
+	// Button Settings
+	'vertical' 			=> 'Vertical count',
+	'horizontal' 		=> 'Horizontal count',
+	'none' 				=> 'No count',
+	
+	'tweet-text' 		=> 'Tweet text',
+	'tweet-text-desc' 	=> 'This is the text that people will include in their tweet when they share from your website:',
+	'tweet-text-1' 		=> 'Post title',
+	'tweet-text-2' 		=> 'Post title + blog name',
+	'tweet-text-3' 		=> 'Blog name',
+	'tweet-text-4' 		=> 'Custom message',
+	'tweet-text-exmp' 	=> 'Example: Check this out!',
+	
+	'url-desc' 			=> 'Suggest a default tweet for users.',
+	'url-1' 			=> 'The URL for the page the button is on.',
+	
+	'language' 			=> 'Language',
+	'language-desc' 	=> 'This is the language that the button will render in on your website. People will see the tweet dialog in their selected language for Twitter.com.',
+	
+	'user' 				=> 'Twitter account',
+	'user-desc' 		=> 'Recommend Twitter account for users to follow after they share content from your website.',
+	'user-1' 			=> 'This user will be @ mentioned in the suggested tweet',
+	
+	'recommend' 		=> 'Recommend Twitter user',
+	'recommend-desc' 	=> 'Recommend another Twitter account for users to follow after they share content from your website.',
+	'recommend-desc-1' 	=> 'Recommended Twitter user description',
+	
+	'position' 			=> 'Button position',
+	'position-desc' 	=> 'This option sets the position that the Twitter Button will be displayed at. ',
+	'position-1' 		=> 'Before the content',
+	'position-2' 		=> 'After the content',
+	'position-3' 		=> 'Both (before + after)',
+	
+	'style' 			=> 'Button style',
+	'style-desc' 		=> 'Additional styles for the div element surrounding the button.',
+	'style-example' 	=> '<b>For example:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Admin panel language',
+	
+	'method' 			=> 'Display method',
+	'automatic' 		=> 'Automatic',
+	'manual' 			=> 'Manual',
+	'manual_desc' 		=> 'With manual method you can put the tweet button wherever you like. All you have to do is to insert this code "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" in your theme.',
+	'shortcode' 		=> 'Shortcode',
+	'shortcode_desc' 	=> 'If you want to disaplay the Twitter Button in certain pages, all you have to do is to insert this shortcode "<strong>[twitter-button]</strong>" in your blog post or page.',
+	'example' 			=> 'Example:',
+	
+	'exclude' 			=> 'Exclude',
+	'exclude-desc' 		=> 'Exclude Twitter button from certain pages:',
+	'exclude-1' 		=> 'Home page',
+	'exclude-2' 		=> 'Single post',
+	'exclude-3' 		=> 'Page',
+	'exclude-4' 		=> 'Tag',
+	'exclude-5' 		=> 'Category',
+	'exclude-6' 		=> 'Archive - date',
+	'exclude-7' 		=> 'Archive - year',
+	'exclude-8' 		=> 'Archive - month',
+	'exclude-9' 		=> 'Archive - day',
+	'exclude-10' 		=> 'Archive - time',
+	'exclude-11' 		=> 'Search',
+	'exclude-12' 		=> 'RSS',
+	
+	'shorturl' 			=> 'URL shortening service',
+	'shorturl-desc' 	=> 'Choose the service to shorten the links shared through the tweet button <em>(default is Twitter\'s t.co)</em>.',
+	
+	// Restore
+	'restore' 			=> 'Restore',
+	'restore-desc' 		=> 'Use this option to restore all the default settings.',
+	
+	// FYI
+	'author' 			=> 'Plugin Author',
+	'version' 			=> 'Version',
+	'url' 				=> 'Plugin URL',
+	'donate' 			=> 'Donate to this plugin'
+); 
+?>
---
+++ langs/hr.php
@@ -0,0 +1,86 @@
+<?php
+// Hrvatski language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Postavke dugmeta',
+	'plugin' 			=> 'Postavke plugina',
+	'saved' 			=> 'Postavke su snimljene.',
+	'update' 			=> 'Potvrdi',
+	
+	// Button Settings
+	'vertical' 			=> 'Vertikalno postavljen',
+	'horizontal' 		=> 'Horizontalno postavljen',
+	'none' 				=> 'Nije postavljen',
+	
+	'tweet-text' 		=> 'Tweet tekst',
+	'tweet-text-desc' 	=> 'Ovo jetekst koji ljudi stavljaju u svoj tweet kada dijele s vaše stranice:',
+	'tweet-text-1' 		=> 'Naslov članka',
+	'tweet-text-2' 		=> 'Naslov članka + ime bloga',
+	'tweet-text-3' 		=> 'Ime bloga',
+	'tweet-text-4' 		=> 'prilagođeno',
+	'tweet-text-exmp' 	=> 'Primjer: Pogledajte ovo!',
+	
+	'url-desc' 			=> 'Predloži zadan tweet za korisnike.',
+	'url-1' 			=> 'The URL for the page the button is on.',
+	
+	'language' 			=> 'Jezik',
+	'language-desc' 	=> 'Ovo je jezik koji će dugme generirati za tweet. Ljudi će vidjeti tweet u njihovom odabranom jeziku za Twitter.com.',
+	
+	'user' 				=> 'Twitter račun',
+	'user-desc' 		=> 'Preporučeni Twitter račun za korisnike da slijede nakon što se podijeli članak sa vaše web stranice.',
+	'user-1' 			=> 'Ovo će pisati nakon @ znaka u preporučenom tweetu',
+	
+	'recommend' 		=> 'Preporučio Twitter korisnik',
+	'recommend-desc' 	=> 'Preporučite dodatnom Twitter račun za korisnike da slijede nakon što se podijeli članak sa vaše web stranice.',
+	'recommend-desc-1' 	=> 'Opis preporučenog Twitter računa',
+	
+	'position' 			=> 'Pozicija dugmeta',
+	'position-desc' 	=> 'Ova opcija definira gdje da se postavi dugme u članku. ',
+	'position-1' 		=> 'Prije teksta',
+	'position-2' 		=> 'Nakon teksta',
+	'position-3' 		=> 'Oboje (prije + kasnije)',
+	
+	'style' 			=> 'Stil dugmeta',
+	'style-desc' 		=> 'Dodatni stil za div elemente okolo dugmeta.',
+	'style-example' 	=> '<b>Primjer:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Jezik administracije',
+	
+	'method' 			=> 'Metoda prikazivanja',
+	'automatic' 		=> 'Automatski',
+	'manual' 			=> 'Ručno',
+	'manual_desc' 		=> 'S ručnom metodom dugme možete staviti gdje poželite. Sve što trebate napraviti je ubaciti kod "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" u vašu temu.',
+	'shortcode' 		=> 'Kratki kod',
+	'shortcode_desc' 	=> 'Ako želite prikazati Twitter dugme samo na nekim stranicama, sve što morate napraviti je ubaciti ovaj kratki kod "<strong>[twitter-button]</strong>" u članak ili stranicu na vašem blogu.',
+	'example' 			=> 'Primjer:',
+	
+	'exclude' 			=> 'Izuzeće',
+	'exclude-desc' 		=> 'Izuzmite Twitter dugme s određenih stranica:',
+	'exclude-1' 		=> 'Početna stranica',
+	'exclude-2' 		=> 'Pojedinačni članak',
+	'exclude-3' 		=> 'Stranica',
+	'exclude-4' 		=> 'Tag',
+	'exclude-5' 		=> 'Kategorija',
+	'exclude-6' 		=> 'Arhiva - datum',
+	'exclude-7' 		=> 'Arhiva - godina',
+	'exclude-8' 		=> 'Arhiva - mjesec',
+	'exclude-9' 		=> 'Arhiva - dan',
+	'exclude-10' 		=> 'Arhiva - vrijeme',
+	'exclude-11' 		=> 'Pretraga',
+	'exclude-12' 		=> 'RSS',
+	
+	'shorturl' 			=> 'Servis skraćivanja URL adresa',
+	'shorturl-desc' 	=> 'Odaberite servis za skraćivanje URL adresa <em>(standardno je Twitterov t.co)</em>.',
+	
+	// Restore
+	'restore' 			=> 'Povrati',
+	'restore-desc' 		=> 'Ovu opciju koristite da vratite postavke na standardne vrijednosti.',
+	
+	// FYI
+	'author' 			=> 'Autor plugina',
+	'version' 			=> 'Verzija',
+	'url' 				=> 'Web stranica plugina',
+	'donate' 			=> 'Donirajte ovom pluginu'
+); 
+?>
