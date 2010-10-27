92e048eb9eae4f6977d2bbb0df843821d80ddfe2
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 14:40:04 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/tags/0.2.8@304979 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/fr.php
@@ -0,0 +1,59 @@
+<?php
+// French language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Param&egrave;tres du bouton',
+	'plugin' 			=> 'Param&egrave;tres du plugin',
+	'saved' 			=> 'Paramètres sauvegard&eacute;s.',
+	'update' 			=> 'Soumettre',
+	// Button Settings
+	'vertical' 			=> 'Compteur vertical',
+	'horizontal' 		=> 'Compteur horizontal',
+	'none' 				=> 'Aucun compteur',
+	'tweet-text' 		=> 'Texte du Tweet',
+	'tweet-text-desc' 	=> 'Texte &agrave; inclure dans le re-tweet lorsqu&apos;un utilisateur clique sur le bouton :',
+	'tweet-text-1' 		=> 'Titre de l&apos;article',
+	'tweet-text-2' 		=> 'Titre de l&apos;article + nom du blog',
+	'tweet-text-3' 		=> 'Nom du blog',
+	'tweet-text-4' 		=> 'Message personnalis&eacute;',
+	'tweet-text-exmp' 	=> 'Exemple : faites tourner !',
+	'url-desc' 			=> 'Sugg&eacute;rer un tweet par d&eacute;faut pour les utilisateurs.',
+	'url-1' 			=> 'URL de la page sur lequel se trouve le bouton.',
+	'language' 			=> 'Langage',
+	'language-desc' 	=> 'Choisissez la langue d&apos;affichage du bouton.',
+	'user' 				=> 'Compte Twitter',
+	'user-desc' 		=> 'Compte Twitter &agrave; suivre apr&egrave;s le re-tweet depuis votre site Web.',
+	'user-1' 			=> 'Cet utilisateur sera @ mentionn&eacute; dans le tweet sugg&eacute;r&eacute;.',		'recommend' 		=> 'Utilisateur Twitter recommandé',	'recommend-desc' 	=> 'Recommandez un autre compte Twitter pour les utilisateurs après avoir re-tweeté vos articles.',	'recommend-desc-1' 	=> 'Description de l\'utilisateur Twitter recommandé',		'position' 			=> 'Position du bouton',	'position-desc' 	=> 'Cette option définit la position du bouton Twitter.',	'position-1' 		=> 'Avant le contenu',	'position-2' 		=> 'Après le contenu',	'position-3' 		=> 'Tous les deux (avant + après)',		'style' 			=> 'Style du bouton',	'style-desc' 		=> 'Styles additionnels pour la balise div entourant le bouton.',	'style-example' 	=> '<b>Par exemple:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	// Plugin Settings
+	'admin-lang' 		=> 'Langue du panneau d&apos;administration',	
+	'method' 			=> 'M&eacute;thode d&apos;affichage',
+	'automatic' 		=> 'Automatique',
+	'manual' 			=> 'Manuelle',
+	'manual_desc' 		=> 'Avec la m&eacute;thode manuelle, vous pouvez ins&eacute;rer le bouton o&ugrave; vous le souhaitez.Il suffit d&apos;ajouter ce bout de code "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" dans votre th&egrave;me.',
+	'shortcode' 		=> 'Shortcode',
+	'shortcode_desc' 	=> 'Si vous souhaitez afficher le bouton Twitter dans certaines pages, il vous suffit d&apos;utiliser le shortcode suivant "<strong>[twitter-button]</strong>" dans un article ou une page.',
+	'example' 			=> 'Exemple:',
+	'exclude' 			=> 'Exclure',
+	'exclude-desc' 		=> 'Exclure le bouton Twitter de certaines pages : ',
+	'exclude-1' 		=> 'Page d&apos;accueil',
+	'exclude-2' 		=> 'Article',
+	'exclude-3' 		=> 'Page',
+	'exclude-4' 		=> 'Tag',
+	'exclude-5' 		=> 'Cat&eacute;gorie',
+	'exclude-6' 		=> 'Archive - date',
+	'exclude-7' 		=> 'Archive - ann&eacute;e',
+	'exclude-8' 		=> 'Archive - mois',
+	'exclude-9' 		=> 'Archive - jour',
+	'exclude-10' 		=> 'Archive - heure',
+	'exclude-11' 		=> 'Rechercher',
+	'exclude-12' 		=> 'RSS',	'shorturl' 			=> 'Raccourcisseur d\'URL',	'shorturl-desc' 	=> 'Choisissez le service pour raccourcir les liens partagés via le bouton de re-tweet <em>(par défaut, Twitter\'s t.co)</em>.',	
+	// Restore
+	'restore' 			=> 'Restaurer',
+	'restore-desc' 		=> 'Restaurer les param&egrave;tres par d&eacute;faut.',
+	// FYI
+	'author' 			=> 'Auteur du plugin',
+	'version' 			=> 'Version',
+	'url' 				=> 'URL du plugin',
+	'donate' 			=> 'Faire un don'
+); 
+?>
---
+++ langs/it.php
@@ -0,0 +1,86 @@
+﻿<?php
+// Italian language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Impostazioni del Pulsante',
+	'plugin' 			=> 'Impostazioni del Plugin',
+	'saved' 			=> 'Impostazioni salvate',
+	'update' 			=> 'Invia',
+	
+	// Button Settings
+	'vertical' 			=> 'Conteggio verticale',
+	'horizontal' 		=> 'Conteggio Orizzontale',
+	'none' 				=> 'Nessun conteggio',
+	
+	'tweet-text' 		=> 'Testo Tweet',
+	'tweet-text-desc' 	=> 'Questo sara il testo che la gente includera nei loro tweet quando condivideranno dal tuo sito web:',
+	'tweet-text-1' 		=> 'Titolo dell\'articolo',
+	'tweet-text-2' 		=> 'Titolo dell\'articolo + il nome del blog',
+	'tweet-text-3' 		=> 'Nome Blog',
+	'tweet-text-4' 		=> 'Messaggio personalizzato',
+	'tweet-text-exmp' 	=> 'Esempio: Controlla questo fuori!',
+	
+	'url-desc' 			=> 'Suggerisci un tweet di default per gli utenti.',
+	'url-1' 			=> 'L\'url della pagina del bottone in.',
+	
+	'language' 			=> 'Lingue',
+	'language-desc' 	=> 'Questo il linguaggio che il pulsante renderа sul vostro sito. La gente vedrа la finestra di dialogo tweet nel linguaggio selezionato per Twitter.com.',
+	
+	'user' 				=> 'Twitter account',
+	'user-desc' 		=> 'Raccomanda account di Twitter da seguire dopo che condividono il contenuto dal tuo sito.',
+	'user-1' 			=> 'Questo utente sarа @ mentioned nel tweet suggerito',
+	
+	'recommend' 		=> 'Raccomanda Twitter Account',
+	'recommend-desc' 	=> 'Consiglia un altro account di Twitter per gli utenti da seguire dopo che condividono il contenuto dal tuo sito.',
+	'recommend-desc-1' 	=> 'Descrizione all\'account Twitter consigliato',
+	
+	'position' 			=> 'Posizione Bottone',
+	'position-desc' 	=> 'Questa opzione imposta la posizione dove il pulsante di Twitter sarа visualizzato. ',
+	'position-1' 		=> 'Prima del coontenuto',
+	'position-2' 		=> 'Dopo del contenuto',
+	'position-3' 		=> 'Entrambi (prima e dopo del contenuto)',
+	
+	'style' 			=> 'Stile del pulsante',
+	'style-desc' 		=> 'Aggiungi stile  per l\'elemento div che circonda il pulsante.',
+	'style-example' 	=> '<b>Per esempio:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Linguaggio del pannello amministrativo',
+	
+	'method' 			=> 'Metodo di visualizzazione',
+	'automatic' 		=> 'Automatico',
+	'manual' 			=> 'Manuale',
+	'manual_desc' 		=> 'Con il metodo manuale si puт mettere il pulsante tweet dove piщ ci piace. Tutto quello che dovete fare и inserire questo codice "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" nel vostro tema.',
+	'shortcode' 		=> 'Shortcode',
+	'shortcode_desc' 	=> 'Se si desidera visualizzare il pulsante Twitter in certe pagine, tutto quello che dovete fare и inserire questo shortcode "<strong>[twitter-button]</strong>" nella tua pagina o nel tuo articolo.',
+	'example' 			=> 'Esempio:',
+	
+	'exclude' 			=> 'Escludi',
+	'exclude-desc' 		=> 'Escludi il pulsante di Twitter da certe pagine:',
+	'exclude-1' 		=> 'Home page',
+	'exclude-2' 		=> 'Singolo post',
+	'exclude-3' 		=> 'Pagina',
+	'exclude-4' 		=> 'Tag',
+	'exclude-5' 		=> 'Categoria',
+	'exclude-6' 		=> 'Archivio - data',
+	'exclude-7' 		=> 'Archivio - anno',
+	'exclude-8' 		=> 'Archivio - mese',
+	'exclude-9' 		=> 'Archivio - giorno',
+	'exclude-10' 		=> 'Archivio - tempo',
+	'exclude-11' 		=> 'Cerca',
+	'exclude-12' 		=> 'RSS',
+	
+	'shorturl' 			=> 'Servizio accorciamento Url',
+	'shorturl-desc' 	=> 'Scegli il servizio di accorciamento link condivisibili attraverso il pulsante <em>(default is Twitter\'s t.co)</em>.',
+	
+	// Restore
+	'restore' 			=> 'Ripristina',
+	'restore-desc' 		=> 'Utilizzare questa opzione per ripristinare tutte le impostazioni predefinite.',
+	
+	// FYI
+	'author' 			=> 'Autore',
+	'version' 			=> 'Versione',
+	'url' 				=> 'URL del plugin',
+	'donate' 			=> 'Contribuisci al plugin donando una piccola somma con PayPal'
+); 
+?>
