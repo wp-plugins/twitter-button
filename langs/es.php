d58108d6e93f5020c662ee2a668db2a9df921c6b
svil4ok <svil4ok@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 14:31:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitter-button/trunk@304967 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ langs/es.php
@@ -0,0 +1,85 @@
+<?php
+// Spanish language pack
+$GLOBALS['tb_lang'] = array (
+	// Basic
+	'button' 			=> 'Configuraci&oacute;n del bot&oacute;n',
+	'plugin' 			=> 'Configuraci&oacute;n del plugin',
+	'saved' 			=> 'Configuraci&oacute;n guardada.',
+	'update' 			=> 'Enviar',
+	
+	// Button Settings
+	'vertical' 			=> 'Contador vertical',
+	'horizontal' 		=> 'Contador horizontal',
+	'none' 				=> 'Sin contador',
+	
+	'tweet-text' 		=> 'Texto del tweet',
+	'tweet-text-desc' 	=> 'Este es el texto que los usuarios incluir&aacute;n en su tweet cuando compartan desde tu sitio:',
+	'tweet-text-1' 		=> 'T&iacute;tulo de la entrada',
+	'tweet-text-2' 		=> 'T&iacute;tulo de la entrada + nombre del blog',
+	'tweet-text-3' 		=> 'Nombre del blog',
+	'tweet-text-4' 		=> 'Mensaje personalizado',
+	'tweet-text-exmp' 	=> 'Ejemplo: &iexcl;Echa un vistazo a esto!',
+	
+	'url-desc' 			=> 'Sugerir un tweet por defecto a los usuarios.',
+	'url-1' 			=> 'URL de la p&aacute;gina en la que se encuentra el bot&oacute;n.',
+	
+	'language' 			=> 'Idioma',
+	'language-desc' 	=> 'Este es el idioma en el que se mostrar&aacute; el bot&oacute;n en tu sitio. Los usuarios veran el cuadro de dialogo del tweet en el idioma que hayan seleccionado en Twitter.com.',
+	
+	'user' 				=> 'Cuenta de Twitter',
+	'user-desc' 		=> 'Recomendar seguir cuenta de Twitter a los usuarios que comparten contenido desde el sitio.',
+	'user-1' 			=> 'Este usuario ser&aacute; @ mencionado en el tweet sugerido',
+	
+	'recommend' 		=> 'Recomendar usuario de Twitter',
+	'recommend-desc' 	=> 'Recomendar a los usuarios seguir otra cuenta de Twitter despu&eacute;s de compartir contenido desde tu sitio.',
+	'recommend-desc-1' 	=> 'Descripci&oacute;n del usuario recomendado',
+	
+	'position' 			=> 'Posici&oacute;n del bot&oacute;n',
+	'position-desc' 	=> 'Esta opci&oacute;n fija la posici&oacute;n en la que ser&aacute; mostrado el boton.',
+	'position-1' 		=> 'Antes del contenido',
+	'position-2' 		=> 'Despu&eacute;s del contenido',
+	'position-3' 		=> 'Ambos (antes y despu&eacute;s)',
+	
+	'style' 			=> 'Estilo del bott&oacute;n',
+	'style-desc' 		=> 'Estilos adicionales para el elemento div en el que se encuentra el bot&oacute;n',
+	'style-example' 	=> '<b>Por ejemplo:</b><br /><br /> float: left; margin: 5px; border: 1px solid #ccc;',
+	
+	// Plugin Settings
+	'admin-lang' 		=> 'Idioma del panel de administracion',
+	
+	'method' 			=> 'M&eacute;todo de visualizaci&oacute;n',
+	'automatic' 		=> 'Autom&aacute;tico',
+	'manual' 			=> 'Manual',
+	'manual_desc' 		=> 'Con el m&eacute;todo manual puedes poner el boton de Twitter donde quieras. Todo lo que tienes que hacer es insertar este c&oacute;digo "<strong>&lt;?php echo twitter_button(); ?&gt;</strong>" en tu tema.',
+	'shortcode' 		=> 'Shortcode',
+	'shortcode_desc' 	=> 'Si quieres mostrar el bot&oacute;n de Twitter en ciertas p&aacute;ginas, todo lo que tienes que hacer es insertar este shortcode "<strong>[twitter-button]</strong>" en tu entrada o p&aacute;gina.',
+	'example' 			=> 'Ejemplo:',
+	'exclude' 			=> 'Excluir',
+	'exclude-desc' 		=> 'No incluir el boton de Twitter en ciertas p&aacute;ginas:',
+	'exclude-1' 		=> 'P&aacute;gina de inicio',
+	'exclude-2' 		=> 'Entrada individual',
+	'exclude-3' 		=> 'P&aacute;gina',
+	'exclude-4' 		=> 'Etiqueta',
+	'exclude-5' 		=> 'Categoria',
+	'exclude-6' 		=> 'Archivo - fecha',
+	'exclude-7' 		=> 'Archivo - a&ntilde;o',
+	'exclude-8' 		=> 'Archivo - mes',
+	'exclude-9' 		=> 'Archive - d&iacute;a',
+	'exclude-10' 		=> 'Archive - hora',
+	'exclude-11' 		=> 'B&uacute;squeda',
+	'exclude-12' 		=> 'RSS',
+	
+	'shorturl' 			=> 'Servicio acortador de URLs',
+	'shorturl-desc' 	=> 'Elige el servicio para acortar los enlaces que sean compartidos a trav&eacute;s del bot&oacute;n de tweet <em>(t.co de Twitter, por defecto)</em>',
+	
+	// Restore
+	'restore' 			=> 'Restaurar',
+	'restore-desc' 		=> 'Utiliza esta opci&oacute;n para volver a los valores por defecto.',
+	
+	// FYI
+	'author' 			=> 'Autor del Plugin',
+	'version' 			=> 'Versi&oacute;n',
+	'url' 				=> 'P&aacute;gina del plugin',
+	'donate' 			=> 'Realizar una donaci&oacute;n'
+); 
+?>
